c01b5884217fbeaeed588aa6e7a807a7205b69b7
Daniel Rybe <danilrybakov249@gmail.com>
2022-03-15 04:50:55 +0700

    Fixed

--- Examples.hs
+++ Examples.hs
@@ -70,28 +70,3 @@ usingGroups = do
 
     group rects -- now the transformations ate applied to rects
     translateM 200 0
-
-
-combinedExamples :: Picture
-combinedExamples = render $ do -- 'render' extracts the picture from the Draw monad
-    renderM simple -- 'renderM' renders the picture and draws it
-    scaleM 0.5 0.5
-    translateM (-width / 4) (height / 4)
-
-    renderM simpleRefactored
-    scaleM 0.5 0.5
-    translateM (width / 4) (height / 4)
-
-    renderM usingIds
-    scaleM 0.5 0.5
-    translateM (-width / 4) (-height / 4)
-
-    renderM usingGroups
-    scaleM 0.5 0.5
-    translateM (width / 4) (-height / 4)
-
-    -- some framing
-    r1 <- rectangleSolidM 5 height
-    r2 <- rectangleSolidM width 5
-    group [r1, r2]
-    colorM black
--- Main.hs
+++ Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import           Draw
 import           Examples
+import           Graphics.Gloss
 
 bg :: Color
 bg = makeColorI 150 150 150 255
@@ -13,6 +14,29 @@ height :: Float
 window :: Display
 window = InWindow "Examples" (round width, round height) (300, 150)
 
+combinedExamples :: Picture
+combinedExamples = render $ do -- 'render' extracts the picture from the Draw monad
+    renderM simple -- 'renderM' renders the picture and draws it
+    scaleM 0.5 0.5
+    translateM (-width / 4) (height / 4)
+
+    renderM simpleRefactored
+    scaleM 0.5 0.5
+    translateM (width / 4) (height / 4)
+
+    renderM usingIds
+    scaleM 0.5 0.5
+    translateM (-width / 4) (-height / 4)
+
+    renderM usingGroups
+    scaleM 0.5 0.5
+    translateM (width / 4) (-height / 4)
+
+    -- some framing
+    r1 <- rectangleSolidM 5 height
+    r2 <- rectangleSolidM width 5
+    group [r1, r2]
+    colorM black
+
 main :: IO ()
 main = display window bg combinedExamples
-
